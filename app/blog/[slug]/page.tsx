d69f183503a370acac5148156ba68889d5045b08
thriveuniverse <littlehousefrance@gmail.com>
2026-01-27 14:56:52 +0100

    markdown working on blogs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,3 @@
-// app/blog/[slug]/page.tsx
 import fs from 'node:fs';
 import path from 'node:path';
 import matter from 'gray-matter';
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
-// next.config.mjs
 import remarkGfm from 'remark-gfm';
+import createNextMdx from '@next/mdx';
 
-export default {
+const withMDX = createNextMdx({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], // enables .mdx files
-
-  
 };
+
+export default withMDX(nextConfig);
